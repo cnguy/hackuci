32b221b424f55f4e6b3964a82be39d09fa5895ec
cnguy <chautnguyen96@gmail.com>
2019-09-30 12:37:24 -0700

    run prettier

--- UserModel.js
+++ UserModel.js
@@ -1,25 +1,21 @@
-var validator  = require('validator')
+var validator = require("validator");
 
 var UserModel = {};
 
 var users = {};
 
 // Adds a user with email and password, and passes success/failure to callback
-UserModel.addUser = function(email, password, callback){
-    if(!email || !validator.isEmail(email)){
+UserModel.addUser = function(email, password, callback) {
+    if (!email || !validator.isEmail(email)) {
         callback(false, "Email is ill-formed");
-    }
-    else if(email in users){
+    } else if (email in users) {
         callback(false, "Email already exists");
-    }
-    else if(!password){
+    } else if (!password) {
         callback(false, "Password is ill-formed");
-    }
-    else{
+    } else {
         users[email] = { password };
         callback(true);
     }
 };
 
 module.exports = UserModel;
-
